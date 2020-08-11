00042cb344d3d48204942691314f465e1ea55e4c
Gabriel D. Sulé <gabrielsule@gmail.com>
2020-08-11 10:55:16 -0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,26 @@
+import subprocess
+
+def getWifi():
+    res = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'])
+    res = res.decode('utf-8').split('\n')
+
+    for i in res:
+        if 'Perfil de todos los usuarios' in i:
+            wifi = i.split(':')[1]
+            print(wifi)
+
+
+def getPwd(_wifi):
+    res = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', _wifi, 'key=clear'])
+    res = res.decode('utf-8', errors="backslashreplace").split('\n')
+
+    for i in res:
+        if 'Contenido de la clave' in i:
+            pwd = i.split(':')[1]
+            print(pwd)
+
+
+
+getWifi()
+
+# getPwd("nombre de la red")
